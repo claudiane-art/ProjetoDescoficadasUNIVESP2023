167d286bba00c472b4a69262f97ae1e8913d0f57
Claudiane Amaral <79753765+claudiane-art@users.noreply.github.com>
2023-06-11 09:54:40 -0300

    Add files via upload

---
+++ Tarefa15_variaveis/operacoes.js
@@ -0,0 +1,13 @@
+var x = 8
+var y = 7
+document.write("A <strong>soma</strong> entre ", x, " e ", y, " é igual a: ", x+y, "<br>");
+document.write("A <strong>divisão</strong> entre ", x, " e ", y, " é igual a: ", x/y, "<br>");
+document.write("A <strong>multiplicação</strong> entre ", x, " e ", y, " é igual a: ", x*y, "<br>");
+
+if (x!=y){
+    document.write("O ", x, " e ", y, " são <strong>diferentes.</strong> ");
+} else {
+    document.write("O ", x, " e ", y, " são <strong>iguais.</strong> ");
+}
+
+
